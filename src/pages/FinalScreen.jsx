53b2d80d9dbc2872b61da6195db16a4d0776d845
Berkin <103454964+berkinkinay@users.noreply.github.com>
2023-08-07 00:44:36 +0300

    -v2

--- src/pages/FinalScreen.jsx
+++ src/pages/FinalScreen.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router'
 import { useDispatch, useSelector } from 'react-redux'
 import { handleAmountChange, handleScoreChange } from '../redux/actions'
+import { BsFillCaretLeftFill } from 'react-icons/bs';
 
 const FinalScreen = () => {
   const dispatch = useDispatch();
@@ -14,15 +15,20 @@ const FinalScreen = () => {
     navigate('/')
   };
   return (
-    <div className='flex flex-col items-center justify-center h-56 w-56 gap-10 bg-black rounded-xl mt-20'>
-      <h1 className='flex text-base text-white'> 
-        Final Score  {score}
-      </h1>
-      <button className='bn632-hover bn24' onClick={handleBackToSettings} variant='outlined'>
-          Back to settings!
-      </button>
+    <div class='flex flex-col items-center justify-center h-full w-full gap-10 p-10 rounded-xl'>
+      <div class='flex flex-col items-center justify-center'>
+        <h1 class='flex text-base font-bold text-slate-900'> 
+          Final Score  {score}
+        </h1>
+        <text class='flex text-base font-semibold text-slate-700'> 
+          Back to question settings page, create and start the new test!
+        </text>
+        <button class='bn632-hover bn24' onClick={handleBackToSettings} variant='outlined'>
+            <BsFillCaretLeftFill class='flex w-full items-center hover:text-black' size='1.5rem' />
+        </button>
+      </div>
     </div>
   )
 }
 
-export default FinalScreen
+export default FinalScreen;
